54abe21f17a6d09f67eed725795824940dd8b82a
Erick Zhao <erick@hotmail.ca>
2025-05-13 21:26:11 -0700

    pathToFileUrl

--- src/sign-with-hook.ts
+++ src/sign-with-hook.ts
@@ -1,4 +1,5 @@
 import path from 'node:path';
+import { pathToFileURL } from 'node:url';
 import { HookFunction, InternalHookOptions, InternalSignOptions } from './types.js';
 import { log } from './utils/log.js';
 
@@ -10,7 +11,7 @@ async function getHookFunction(options: InternalHookOptions): Promise<HookFuncti
   }
 
   if (options.hookModulePath) {
-    const module = await import(path.resolve(options.hookModulePath));
+    const module = await import(pathToFileURL(path.resolve(options.hookModulePath)).toString());
 
     if (module.default) {
       return module.default;
